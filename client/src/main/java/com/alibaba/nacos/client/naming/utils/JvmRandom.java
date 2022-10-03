22d92140c3775672aa77ce4d1cb6a968a567e5d7
chienlungcheung <chienlungcheung@gmail.com>
2022-10-03 20:47:44 +0800

    在 jdk19 下修正编译错误

--- client/src/main/java/com/alibaba/nacos/client/naming/utils/JvmRandom.java
+++ client/src/main/java/com/alibaba/nacos/client/naming/utils/JvmRandom.java
@@ -135,7 +135,7 @@ public final class JvmRandom extends Random {
      * @return the random long
      * @throws IllegalArgumentException when <code>n &lt;= 0</code>
      */
-    public static long nextLong(long n) {
+    public long nextLong(long n) {
         if (n <= 0) {
             throw new IllegalArgumentException(
                 "Upper bound for nextInt must be positive"
